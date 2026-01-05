df812a3c6ddab591d6c661aa01e9ad9fa03be7ce
kawhite81 <151670300+kawhite81@users.noreply.github.com>
2026-01-05 12:47:20 -0500

    Implement BaseWebPart class with render method

--- src/webparts/BaseWebPart/BaseWebPart.ts
+++ src/webparts/BaseWebPart/BaseWebPart.ts
@@ -1,1 +1,17 @@
+export default class BaseWebPart extends BaseClientSideWebPart<IBaseConfig> {
 
+  protected onInit(): Promise<void> {
+    return super.onInit();
+  }
+
+  public render(): void {
+    const element = React.createElement(
+      BaseWebPartComponent,
+      {
+        config: this.properties
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+}
